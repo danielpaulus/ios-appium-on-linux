f723b9b9dd65a5a11a4be7a3e42e6093325310ae
Daniel Paulus <daniel.paulus@icloud.com>
2021-04-01 17:23:54 +0200

    add comments

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+# pass the container image name as a parameter
+# exposes the default appium port 4723 so our automation script can use it.
+# mounts /var/run/usbmuxd into the container, this is the unix domain socket that allows all tools
+# to communicate with iOS devices. 
+# /var/lib/lockdown is where the pairing information is stored, so you don't have to pair your phone
+# everytime, but just once on the host
 docker run -p 4723:4723 -v /var/run/usbmuxd:/var/run/usbmuxd -v /var/lib/lockdown:/var/lib/lockdown $1
--- startAppium.sh
+++ startAppium.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# this is the entrypoint script for our container
+# set up go-iOS to forward the localhost:7777 over USB to WebDriverAgent on 8100
+# Appium will use that port because we tell it to in the desiredCapabilities of our automation
+# script. 
 /ios forward 7777 8100&
+# set up nodeJS with nvm
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+# run appium
 /root/.nvm/versions/node/v14.16.0/bin/appium
